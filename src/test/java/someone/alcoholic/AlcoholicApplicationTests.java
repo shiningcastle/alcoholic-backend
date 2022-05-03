4e6ae36b35eccb607c18c07bc8bf9a1edd24f041
Lee SeungHun <q6801234@gmail.com>
2022-05-03 16:49:01 +0900

    feat: 초기 세팅

---
+++ src/test/java/someone/alcoholic/AlcoholicApplicationTests.java
@@ -0,0 +1,13 @@
+package someone.alcoholic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
