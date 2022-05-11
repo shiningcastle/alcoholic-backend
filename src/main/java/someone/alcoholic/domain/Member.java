24f03e3653983028c93a34ce648e4f39edd8a137
Lee SeungHun <q6801234@gmail.com>
2022-05-11 16:31:38 +0900

    feat: member 생성자 추가

--- src/main/java/someone/alcoholic/domain/Member.java
+++ src/main/java/someone/alcoholic/domain/Member.java
@@ -1,14 +1,18 @@
 package someone.alcoholic.domain;
 
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.sql.Timestamp;
 
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+    @Id
     private int id;
     private String email;
     private String password;
@@ -17,4 +21,12 @@ public class Member {
     private String role;
     private Timestamp createdDate;
     private Timestamp passwordUpdatedDate;
+
+    public Member(String email, String password, String nickname, String provider, String role) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.provider = provider;
+        this.role = role;
+    }
 }
