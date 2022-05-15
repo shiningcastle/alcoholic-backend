1ece930b8de114a83e9a6ad37f59d499b92e2368
Lee SeungHun <q6801234@gmail.com>
2022-05-16 00:37:28 +0900

    feat: signup 메서드 구현

--- src/main/java/someone/alcoholic/service/MemberService.java
+++ src/main/java/someone/alcoholic/service/MemberService.java
@@ -6,5 +6,4 @@ import someone.alcoholic.dto.MemberSignupDto;
 
 public interface MemberService {
     Member signup(MemberSignupDto signupDto);
-    Member login(MemberLoginDto loginDto);
 }
--- src/main/java/someone/alcoholic/service/MemberServiceImpl.java
+++ src/main/java/someone/alcoholic/service/MemberServiceImpl.java
@@ -1,25 +1,36 @@
 package someone.alcoholic.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import someone.alcoholic.domain.Member;
 import someone.alcoholic.dto.MemberLoginDto;
 import someone.alcoholic.dto.MemberSignupDto;
+import someone.alcoholic.enums.ExceptionEnum;
+import someone.alcoholic.exception.CustomRuntimeException;
 import someone.alcoholic.repository.MemberRepository;
 
+import java.util.Optional;
+
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
-
+    @Autowired
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Member signup(MemberSignupDto signupDto) {
-        return null;
+        memberRepository.findByEmail(signupDto.getEmail())
+                .ifPresent((mem) -> { throw new CustomRuntimeException(ExceptionEnum.USER_ALREADY_EXIST);});
+        memberRepository.findByEmail(signupDto.getNickname())
+                .ifPresent((mem) -> { throw new CustomRuntimeException(ExceptionEnum.NICKNAME_ALREADY_EXIST);});
+
+        return memberRepository.save(Member.createLocalMember(
+                signupDto.getEmail(), passwordEncoder.encode(signupDto.getPassword()), signupDto.getNickname()));
     }
 
-    @Override
-    public Member login(MemberLoginDto loginDto) {
-        return null;
-    }
 }
