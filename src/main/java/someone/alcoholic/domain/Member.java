4e88600c411c0842a238b301b99c9f2bcdf7042d
Lee SeungHun <q6801234@gmail.com>
2022-05-16 00:30:19 +0900

    feat: createLocalMember static 메서드 생성

--- src/main/java/someone/alcoholic/domain/Member.java
+++ src/main/java/someone/alcoholic/domain/Member.java
@@ -3,24 +3,28 @@ package someone.alcoholic.domain;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 import someone.alcoholic.enums.Provider;
 import someone.alcoholic.enums.Role;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(nullable = false)
     private String email;
 
-    @Column(length = 45, nullable = false)
+    @Column(length = 100, nullable = false)     // 인코딩 하기 때문에 더 커야됨
     private String password;
 
     @Column(length = 45, nullable = false)
@@ -35,16 +39,26 @@ public class Member {
     private Role role;
 
     @Column(name = "created_date", nullable = false)
+    @CreationTimestamp
     private Timestamp createdDate;
 
     @Column(name = "password_updated_date")
+    @CreationTimestamp
     private Timestamp passwordUpdatedDate;
 
-    public Member(String email, String password, String nickname, String provider, String role) {
+    public Member(String email, String password, String nickname,
+                  Provider provider, Role role) {
         this.email = email;
         this.password = password;
         this.nickname = nickname;
         this.provider = provider;
         this.role = role;
+        // 3개월 뒤로 설정
+    }
+
+    public static Member createLocalMember(String email, String password, String nickname) {
+        Member member = new Member(email, password, nickname, Provider.LOCAL, Role.USER);
+        member.passwordUpdatedDate = Timestamp.valueOf(LocalDateTime.now().plusMonths(3));
+        return member;
     }
 }
