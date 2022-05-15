0906903c7d97a3f2e22d9ca736eb7de78aecfac4
Lee SeungHun <q6801234@gmail.com>
2022-05-16 00:31:34 +0900

    fix: memberLoginDto의 패키지 변경

--- src/main/java/someone/alcoholic/dto/MemberLoginDto.java
+++ src/main/java/someone/alcoholic/dto/MemberLoginDto.java
@@ -1,7 +1,10 @@
 package someone.alcoholic.dto;
 
+import lombok.Getter;
+
 import javax.validation.constraints.NotBlank;
 
+@Getter
 public class MemberLoginDto {
 
     @NotBlank(message = "E-Mail 빈(공백) 문자열을 허용하지 않습니다.")
