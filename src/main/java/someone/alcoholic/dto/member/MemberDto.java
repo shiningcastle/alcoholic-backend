f12dcd9ee51b4472deec5538005138989a449624
Lee SeungHun <q6801@naver.com>
2022-05-22 01:33:01 +0900

    feat: memberDto에 image, role 필드 추가

--- src/main/java/someone/alcoholic/dto/member/MemberDto.java
+++ src/main/java/someone/alcoholic/dto/member/MemberDto.java
@@ -1,11 +1,23 @@
 package someone.alcoholic.dto.member;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import someone.alcoholic.enums.Role;
+
 import javax.validation.constraints.NotBlank;
 
+@Getter
+@AllArgsConstructor
 public class MemberDto {
+    @NotBlank(message = "nickname 빈(공백) 문자열을 허용하지 않습니다.")
+    private String nickname;
+
     @NotBlank(message = "E-Mail 빈(공백) 문자열을 허용하지 않습니다.")
     private String email;
 
-    @NotBlank(message = "nickname 빈(공백) 문자열을 허용하지 않습니다.")
-    private String nickname;
+    @NotBlank(message = "image 빈(공백) 문자열을 허용하지 않습니다.")
+    private String image;
+
+    @NotBlank(message = "role 빈(공백) 문자열을 허용하지 않습니다.")
+    private Role role;
 }
