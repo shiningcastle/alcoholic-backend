cf21e4cdc215f97a1ccc3c148e299dd69f6979e8
Lee SeungHun <q6801@naver.com>
2022-05-22 01:15:43 +0900

    feat: local 로그인 오류 api return하게 변경

--- src/main/java/someone/alcoholic/security/CustomAccessDeniedHandler.java
+++ src/main/java/someone/alcoholic/security/CustomAccessDeniedHandler.java
@@ -1,9 +1,14 @@
 package someone.alcoholic.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import someone.alcoholic.api.ApiProvider;
+import someone.alcoholic.api.ApiResult;
+import someone.alcoholic.enums.ExceptionEnum;
+import someone.alcoholic.exception.CustomRuntimeException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,8 +20,11 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         //response에 넣기
+        ApiResult<?> error = ApiProvider.error(new CustomRuntimeException(ExceptionEnum.NOT_ALLOWED_ACCESS));
+        ObjectMapper om = new ObjectMapper();
+        response.setCharacterEncoding("UTF-8");
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        response.setStatus(HttpStatus.FORBIDDEN.value());
-        response.getWriter().print("error: 권한 오류");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(om.writeValueAsString(error));
     }
 }
