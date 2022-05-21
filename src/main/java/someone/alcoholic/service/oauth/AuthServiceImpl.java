aa6172b7301a728d67646c8b14c744e9f07f2699
Lee SeungHun <q6801@naver.com>
2022-05-22 01:22:44 +0900

    refactor: getAuthority 메서드 생성

--- src/main/java/someone/alcoholic/service/oauth/AuthServiceImpl.java
+++ src/main/java/someone/alcoholic/service/oauth/AuthServiceImpl.java
@@ -10,17 +10,16 @@ import org.springframework.transaction.annotation.Transactional;
 import someone.alcoholic.domain.member.Member;
 import someone.alcoholic.domain.token.RefreshToken;
 import someone.alcoholic.dto.auth.MemberLoginDto;
+import someone.alcoholic.enums.Age;
 import someone.alcoholic.enums.ExceptionEnum;
 import someone.alcoholic.exception.CustomRuntimeException;
 import someone.alcoholic.repository.member.MemberRepository;
 import someone.alcoholic.repository.token.RefreshTokenRepository;
 import someone.alcoholic.security.AuthToken;
 import someone.alcoholic.security.AuthTokenProvider;
-import someone.alcoholic.service.oauth.AuthService;
 import someone.alcoholic.util.CookieUtil;
 
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 import java.util.UUID;
 
 @Service
@@ -35,27 +34,26 @@ public class AuthServiceImpl implements AuthService {
     public Member login(HttpServletResponse response, MemberLoginDto loginDto) {
         String memberId = loginDto.getId();
         String pw = loginDto.getPassword();
-        Date now = new Date();
 
         Authentication authentication = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(memberId, pw)
-        );
+                new UsernamePasswordAuthenticationToken(memberId, pw));
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        String role = authentication.getAuthorities().stream().findAny()
-                .orElseThrow(() -> new CustomRuntimeException(ExceptionEnum.VALUE_NOT_FOUND))
-                .getAuthority();
-
-        AuthToken accessToken = tokenProvider.createAccessToken(
-                memberId, role, new Date(now.getTime() + tokenProvider.getAccessTokenMaxAge()));
+        String role = getAuthority(authentication);
+        AuthToken accessToken = tokenProvider.createAccessToken(memberId, role);
         UUID refreshTokenId = UUID.randomUUID();
         AuthToken refreshToken = tokenProvider.createRefreshToken(refreshTokenId, memberId);
         refreshTokenRepository.save(new RefreshToken(refreshTokenId, memberId, refreshToken.getToken()));
 
         setCookie(response, accessToken, refreshToken);
-
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new CustomRuntimeException(ExceptionEnum.USER_NOT_EXIST));
+    }
+
+    private String getAuthority(Authentication authentication) {
+        return authentication.getAuthorities().stream().findAny()
+                .orElseThrow(() -> new CustomRuntimeException(ExceptionEnum.VALUE_NOT_FOUND))
+                .getAuthority();
     }
 
     private void setCookie(HttpServletResponse response, AuthToken accessToken, AuthToken refreshToken) {
