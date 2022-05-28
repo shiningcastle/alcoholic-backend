95645535cea7f23b69368e3e58353f11e46b4458
Lee SeungHun <q6801@naver.com>
2022-05-28 17:01:19 +0900

    fix: access token 쿠키 유효 시간 변경

--- src/main/java/someone/alcoholic/enums/CookieExpiryTime.java
+++ src/main/java/someone/alcoholic/enums/CookieExpiryTime.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum CookieExpiryTime {
-    ACCESS_COOKIE_MAX_AGE(3600),
-    REFRESH_COOKIE_MAX_AGE(10800),
+    ACCESS_COOKIE_MAX_AGE(4200),        // 시간 만료보다 10분 더 김 
+    REFRESH_COOKIE_MAX_AGE(10800),      // 토큰 만료와 시간 일치
     NICKNAME_COOKIE_EXPIRY_TIME(1200);
 
     private int value;
