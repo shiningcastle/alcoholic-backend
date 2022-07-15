3af5fe2fb426d5e6d1d3a1b65a17bd56bce0b152
shiningcastle <shiningcastle.dev@gmail.com>
2022-07-16 00:31:29 +0900

    FEAT: Swagger 수정

--- src/main/java/someone/alcoholic/dto/reply/ReplyDto.java
+++ src/main/java/someone/alcoholic/dto/reply/ReplyDto.java
@@ -1,15 +1,24 @@
 package someone.alcoholic.dto.reply;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 import java.sql.Timestamp;
 
 @Getter
 public class ReplyDto {
+
+    @ApiModelProperty(name = "seq", value = "댓글번호", example = "132")
     private Long seq;
+
+    @ApiModelProperty(name = "replyParent", value = "상위 댓글번호", example = "13")
     private Long replyParent;
+
+    @ApiModelProperty(name = "content", value = "댓글내용", example = "오 너무 좋은 글이네요ㅎㅎ 잘봤습니다!")
     private String content;
+
+    @ApiModelProperty(name = "isRoot", value = "최상위 댓글 여부", example = "true")
     private Boolean isRoot;
 
     @JsonFormat(pattern = "yyyy.MM.dd'T'HH:mm:ss", timezone = "Asia/Seoul")
