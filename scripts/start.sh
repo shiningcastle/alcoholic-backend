7be689d84af776b948050d3755793788e1000eac
Lee SeungHun <q6801@naver.com>
2022-07-28 21:36:24 +0900

    fix: 배포 설정 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@ APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
-#TIME_NOW=$(date +%c)
+TIME_NOW=$(date +%c)
 
 # build 파일 복사
 #echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
