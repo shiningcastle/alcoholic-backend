3fee13667036fba16279dde1c2c772980afa4baf
Lee SeungHun <q6801@naver.com>
2022-08-22 00:37:28 +0900

    fix: pageable 표시 오류 수정

--- src/main/java/someone/alcoholic/config/SwaggerConfig.java
+++ src/main/java/someone/alcoholic/config/SwaggerConfig.java
@@ -1,11 +1,19 @@
 package someone.alcoholic.config;
 
+import com.fasterxml.classmate.TypeResolver;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
@@ -18,12 +26,18 @@ import java.util.List;
 import java.util.Set;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableWebMvc
 public class SwaggerConfig {
+
+    private final TypeResolver typeResolver;
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
+                .alternateTypeRules(AlternateTypeRules
+                        .newRule(typeResolver.resolve(Pageable.class), typeResolver.resolve(Page.class))
+                )
                 .consumes(getConsumeContentTypes())
                 .produces(getProduceContentTypes())
                 .securityContexts(Arrays.asList(securityContext()))
@@ -75,4 +89,17 @@ public class SwaggerConfig {
         return produces;
     }
 
+    @Getter
+    @Setter
+    @ApiModel
+    static class Page {
+        @ApiModelProperty(value = "페이지 번호(0..N)")
+        private Integer page;
+
+        @ApiModelProperty(value = "페이지 크기", allowableValues="range[0, 100]")
+        private Integer size;
+
+        @ApiModelProperty(value = "정렬(사용법: 컬럼명,ASC|DESC)")
+        private List<String> sort;
+    }
 }
